6e94204800aeaf98ac67524878f9566540092352
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-01-04 02:51:50 -0300

    Revert "Merge branch 'master' into feature-mediapipe-metho"
    This reverts commit cfabfbbd1b334715214a5a5977afc025beb27c11, reversing
    changes made to 13ed4edc1233985c050bd88a5e4399d9dccdbce2.
    
    reverting merge

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import numpy as np
 import cv2
 import io
 import base64
-import landmark
+import gaze_detector
+import gaze_detector2
 
 app = FastAPI()
 origins = [
@@ -24,8 +25,6 @@ app.add_middleware(
 )
 
 
-
-
 @app.get("/")
 async def hello():
     return {"message": "Server is up and running!"}
@@ -36,7 +35,7 @@ async def predict(file: UploadFile = File(...)):
     contents = await file.read()
     nparr = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    data, image = landmark.predict(img)
+    data, image = gaze_detector.detect(img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     # line that fixed it
@@ -46,5 +45,20 @@ async def predict(file: UploadFile = File(...)):
     res = StreamingResponse(io.BytesIO(encoded_img.tobytes()), media_type="image/png")
     return JSONResponse(data)
 
+
+@app.post("/predict-file-est")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    data, image = gaze_detector2.detect(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # line that fixed it
+    _, encoded_img = cv2.imencode('.PNG', image)
+    #encoded_img = base64.b64encode(encoded_img)
+    headers = (data)
+    res = StreamingResponse(io.BytesIO(encoded_img.tobytes()), media_type="image/png")
+    return JSONResponse(data)
 
 
